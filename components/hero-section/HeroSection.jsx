8aee41b50f16349b2885bbc14e5abd8a6213251d
Jasmyre <lanuzajasmyreandrei@gmail.com>
2024-08-11 19:06:30 +0800

    Fix: hero content

--- components/hero-section/HeroSection.jsx
+++ components/hero-section/HeroSection.jsx
@@ -5,15 +5,13 @@ const HeroSection = () => {
 	return (
 		<section className={styles.hero_section}>
 			<div className={styles.hero_wrapper}>
-				<h1 className="averia-serif-libre-bold">Discover Movies Now</h1>
+				<h1 className="averia-serif-libre-bold">
+					Your Ultimate Movie Discovery Tool
+				</h1>
 				<p className="cinzel">
-					Simplify your movie search experience. Just type in a movie
-					title, and we'll provide you with everything you need to
-					know—release date, language, overview, and ratings. We also
-					show you where you can watch it in your country, making it
-					easy to find the best streaming options. No categories, no
-					hassle, just straightforward and quick access to the movies
-					you love.
+					Explore genres, read reviews, and discover new favorites
+					with just a few clicks. the go-to platform for all your
+					movie searching needs.
 				</p>
 			</div>
 		</section>
